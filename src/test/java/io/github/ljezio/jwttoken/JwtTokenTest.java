eb892ec97447795038c4d171581e1547a3789ca7
EZIO <1141988108@qq.com>
2023-07-25 22:52:51 +0800

    单元测试完善

--- src/test/java/io/github/ljezio/jwttoken/JwtTokenTest.java
+++ src/test/java/io/github/ljezio/jwttoken/JwtTokenTest.java
@@ -1,13 +1,13 @@
 package io.github.ljezio.jwttoken;
 
-import io.github.ljezio.jwttoken.configuration.InterceptorProperties;
+import io.github.ljezio.jwttoken.common.BeanContent;
 import io.github.ljezio.jwttoken.configuration.JwtTokenAutoConfiguration;
 import io.github.ljezio.jwttoken.exception.TokenAlreadyExpiredException;
 import io.github.ljezio.jwttoken.exception.TokenVerifierFailException;
 import io.github.ljezio.jwttoken.interceptor.TokenCheckInterceptor;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -16,19 +16,23 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
 
-@SpringBootTest(classes = JwtTokenAutoConfiguration.class, properties = "jwt-token.interception.enable=true")
+@SpringBootTest(
+        properties = "jwt-token.interception.enable=true",
+        classes = {JwtTokenAutoConfiguration.class, JacksonAutoConfiguration.class}
+)
 class JwtTokenTest {
 
-    private static final InterceptorProperties interceptorProp = new InterceptorProperties();
     private static final Payload payload = new Payload(1234, "ezio");
     private static Token token;
 
-    @BeforeAll
-    public static void create() {
+    @Test
+    public void create() {
         token = JwtToken.create(payload);
         System.out.println("now:\n" + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
         System.out.println("accessToken:\n" + token.accessToken());
         System.out.println("refreshToken:\n" + token.refreshToken());
+        Assertions.assertNotNull(token.accessToken());
+        Assertions.assertNotNull(token.refreshToken());
     }
 
     @Test
@@ -47,11 +51,11 @@ class JwtTokenTest {
 
     @Test
     public void interceptor() {
+        String headerValue = BeanContent.interceptorProp.getHeaderValuePrefix() + token.accessToken();
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addHeader(interceptorProp.getHeader(), interceptorProp.getHeaderValuePrefix() + token.accessToken());
+        request.addHeader(BeanContent.interceptorProp.getHeader(), headerValue);
         TokenCheckInterceptor interceptor = new TokenCheckInterceptor();
         boolean result = interceptor.preHandle(request, new MockHttpServletResponse(), null);
         Assertions.assertTrue(result);
     }
-
 }
